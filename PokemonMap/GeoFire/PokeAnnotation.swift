a8ceec87d77b7aa5f9d2a866188596c9dfe24554
Patrick Gross <patrick.gross@native-instruments.de>
2016-11-11 10:39:01 +0100

    First and last commit

---
+++ PokemonMap/GeoFire/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  PokemonMap
+//
+//  Created by Patrick Gross on 10/11/2016.
+//  Copyright © 2016 Patrick Gross. All rights reserved.
+//
+
+import Foundation
---
+++ PokemonMap/PokemonMap-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokemonMap-Bridging-Header.h
+//  PokemonMap
+//
+//  Created by Patrick Gross on 10/11/2016.
+//  Copyright © 2016 Patrick Gross. All rights reserved.
+//
+
+#ifndef PokemonMap_Bridging_Header_h
+#define PokemonMap_Bridging_Header_h
+
+
+#endif /* PokemonMap_Bridging_Header_h */
